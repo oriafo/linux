23c98c3b9dbcf3a5a0858b75d75aed2d9dcf358a
oriafo <abrahamibharunujele@gmail.com>
2023-04-25 17:37:12 +0100

    sending parameter to shell fuction during runtime

--- shell_scripting/command_line_argument/script_dynamic_Argument.bash
+++ shell_scripting/command_line_argument/script_dynamic_Argument.bash
@@ -9,4 +9,18 @@ echo "All the Arguments are: $@"
 echo "The script itself is: $0"
 echo "All the Arguments are: $*"
 echo "The number of Arguments are $#"
-  
+
+# use echo command to make a newline between the upper code and the code below. The above code is for understanding the concept, while the code below is implementing the concept 
+echo
+
+func_argument(){
+echo "THe first Argument is : $1"
+echo "THe second Argument is: $2"
+echo "THe THird Argument is: $3"
+echo "All the Arguments are: $@"
+echo "The script itself is: $0"
+echo "The number of Argument are $#"
+echo "All the Argument are: $*"
+}
+
+func_argument This scripting is getting more interesting 
