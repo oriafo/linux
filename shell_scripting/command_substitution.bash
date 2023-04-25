d66ace91a88671dd059e24250f5bfa0a877a9d07
oriafo <abrahamibharunujele@gmail.com>
2023-04-25 17:50:26 +0100

    command substitution

--- shell_scripting/command_substitution.bash
+++ shell_scripting/command_substitution.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#This script is used to demonstrate command substitution
+#This script is used to demonstrate command substitution. this is very handy in executing command inside of an expression 
 
 List=`ls`
 Date=`date`
