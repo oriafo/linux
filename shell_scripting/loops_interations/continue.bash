a52f4199dbc67419f65f16d726bb51ad20012bfa
oriafo <abrahamibharunujele@gmail.com>
2023-04-25 17:10:22 +0100

    continue statement

--- shell_scripting/loops_interations/continue.bash
+++ shell_scripting/loops_interations/continue.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#The continue statement causes the current loop to exit rather than the entire loop, unlike break statement to causes the entire loop to exit. this cn be seen in the case of try and except statement during exceptions.
+#The continue statement causes the current loop to exit rather than the entire loop, unlike break statement to causes the entire loop to exit. this can be seen in the case of try and except statement during exceptions.
 
 num="1 2 3 4 5 6"
 
