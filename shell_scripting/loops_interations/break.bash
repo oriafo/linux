ef9bc282e21b25b13bbfd069fa3450181d58fc78
oriafo <abrahamibharunujele@gmail.com>
2023-04-25 17:04:17 +0100

    breaking out from a loop

--- shell_scripting/loops_interations/break.bash
+++ shell_scripting/loops_interations/break.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#The code shows the use of break statement to leave the entire loop
+#The code shows the use of break statement to LEAVE the entire loop unlike the continue statement. 
 
 a=0
 while [ $a -lt 10 ]
