39864912095c606178aec8d098da60d12ce58751
oriafo <abrahamibharunujele@gmail.com>
2023-04-25 16:47:45 +0100

    shell scripting Use_case

---
+++ shell_scripting/use_cases/scan_monitor_network.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#This script helps to scan and monitor small home brand networks using the Ping command where sophisticated monitoring software can become an overhead. it checks for ip that are up in a particular subnet 
+
+is_alive(){ 
+	ping -c 1 $1 > /dev/null
+	[ $? -eq 0 ] && echo The Ip address: $i is up
+}
+
+for i in 192.168.43.{1..255} 
+do
+	is_alive $i & disown
+done
+exit
